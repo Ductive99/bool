ecce2f6d7c56068960f9350caf8aea7ae3f80a7c
El Houssain Souhail <sou.ductive@gmail.com>
2026-06-11 12:38:07 +0200

    Revert "improve(adder): simplify adder function"
    This reverts commit 20c3371b235c54882499c344f75a2d66c2cf6020.

--- src/adder.cpp
+++ src/adder.cpp
@@ -6,19 +6,32 @@
 /*   By: esouhail <ductive99.github.io>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 11:19:53 by esouhail          #+#    #+#             */
-/*   Updated: 2026/06/10 18:23:37 by esouhail         ###   ########.fr       */
+/*   Updated: 2026/06/10 17:00:20 by esouhail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bool.h"
 
 u32 adder(u32 a, u32 b) {
-	while (b != 0) {
-		u32	carry = a & b;
-		a	  = a ^ b;
-		b	  = carry << 1;
+	u32 result = 0;
+	u32 a_bit;
+	u32 b_bit;
+	u32 carry;
+
+	carry = 0;
+	for (int i = 0; i < 32; i++) {
+		if (a >> i == 0 && b >> i == 0 && carry == 0)
+			break;
+		a_bit	  = 1 & (a >> i);
+		b_bit	  = 1 & (b >> i);
+		u32 sum	  = a_bit ^ b_bit;
+		u32 c_out = a_bit & b_bit;
+
+		result |= ((sum ^ carry) << i);
+		carry = c_out | (carry & sum);
 	}
-	return a;
+
+	return result;
 }
 
 void test_adder(void) {
