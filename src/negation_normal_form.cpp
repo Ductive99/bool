96ab13e4da1e58d7fb195473944006d51f43d939
El Houssain Souhail <sou.ductive@gmail.com>
2026-06-30 12:16:30 +0200

    add: XOR transformation to negation normal form

--- src/negation_normal_form.cpp
+++ src/negation_normal_form.cpp
@@ -6,7 +6,7 @@
 /*   By: esouhail <esouhail@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 13:12:01 by esouhail          #+#    #+#             */
-/*   Updated: 2026/06/30 11:59:29 by esouhail         ###   ########.fr       */
+/*   Updated: 2026/06/30 12:15:47 by esouhail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,33 +28,40 @@ std::unique_ptr<ASTNode> transform_to_nnf(const ASTNode *node) {
 
 	if (node->type == NodeType::Implies) {
 		auto new_node = ASTNode::make_binary(
-				NodeType::Or,
-				ASTNode::make_unary(NodeType::Not, node->left->clone()),
-				node->right->clone()
-			);
+			NodeType::Or,
+			ASTNode::make_unary(NodeType::Not, node->left->clone()),
+			node->right->clone());
 		return transform_to_nnf(new_node.get());
 	}
 
 	if (node->type == NodeType::Iff) {
 		auto new_node = ASTNode::make_binary(
-				NodeType::And,
-				ASTNode::make_binary(
-					NodeType::Or, 
-					ASTNode::make_unary(NodeType::Not, node->left->clone()), 
-					node->right->clone()
-				),
-				ASTNode::make_binary(
-					NodeType::Or,
-					node->left->clone(),
-					ASTNode::make_unary(NodeType::Not, node->right->clone())
-				)
-			);
+			NodeType::And,
+			ASTNode::make_binary(
+				NodeType::Or,
+				ASTNode::make_unary(NodeType::Not, node->left->clone()),
+				node->right->clone()),
+			ASTNode::make_binary(
+				NodeType::Or, node->left->clone(),
+				ASTNode::make_unary(NodeType::Not, node->right->clone())));
+		return transform_to_nnf(new_node.get());
+	}
+
+	if (node->type == NodeType::Xor) {
+		auto new_node = ASTNode::make_binary(
+			NodeType::And,
+			ASTNode::make_binary(NodeType::Or, node->left->clone(),
+								 node->right->clone()),
+			ASTNode::make_binary(
+				NodeType::Or,
+				ASTNode::make_unary(NodeType::Not, node->left->clone()),
+				ASTNode::make_unary(NodeType::Not, node->right->clone())));
 		return transform_to_nnf(new_node.get());
 	}
 
 	if (node->is_leaf())
 		return node->clone();
-	
+
 	if (node->type == NodeType::Not) {
 		const ASTNode *child = node->left.get();
 		if (!child)
@@ -65,14 +72,12 @@ std::unique_ptr<ASTNode> transform_to_nnf(const ASTNode *node) {
 				throw InvalidFormulaException();
 			return transform_to_nnf(child->left.get());
 		}
-		
-		if (child->type == NodeType::And || child->type == NodeType::Or)
-		{
+
+		if (child->type == NodeType::And || child->type == NodeType::Or) {
 			auto new_node = ASTNode::make_binary(
 				child->type == NodeType::And ? NodeType::Or : NodeType::And,
 				ASTNode::make_unary(NodeType::Not, child->left->clone()),
-				ASTNode::make_unary(NodeType::Not, child->right->clone())
-			);
+				ASTNode::make_unary(NodeType::Not, child->right->clone()));
 			return transform_to_nnf(new_node.get());
 		}
 
@@ -80,14 +85,10 @@ std::unique_ptr<ASTNode> transform_to_nnf(const ASTNode *node) {
 			return node->clone();
 
 		auto new_child = transform_to_nnf(child);
-        return transform_to_nnf(
-            ASTNode::make_unary(NodeType::Not, std::move(new_child)).get()
-        );
+		return transform_to_nnf(
+			ASTNode::make_unary(NodeType::Not, std::move(new_child)).get());
 	}
 
-	return ASTNode::make_binary(
-			node->type,
-			transform_to_nnf(node->left.get()),
-			transform_to_nnf(node->right.get())
-		);
+	return ASTNode::make_binary(node->type, transform_to_nnf(node->left.get()),
+								transform_to_nnf(node->right.get()));
 }
